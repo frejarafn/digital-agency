04072f322bfa8db8c746dcff33f8de86308392c2
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 11:30:05 +0100

    nadia

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import "../styles/navigation.css";
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import {MdHome} from 'react-icons/md';
-import {TbSearch} from 'react-icons/tb';
+import {HiSearch} from 'react-icons/hi';
 import {AiFillCalendar} from 'react-icons/ai';
 import {IoIosSettings} from 'react-icons/io';
 export default function Navbar() {
@@ -10,7 +10,7 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <NavLink to="/" end><MdHome/></NavLink>
-            <NavLink to="/discover"><TbSearch/></NavLink>
+            <NavLink to="/discover"><HiSearch/></NavLink>
             <NavLink to="/plan"><AiFillCalendar/></NavLink>
             <NavLink to="/settings"><IoIosSettings/></NavLink>
         </nav>
