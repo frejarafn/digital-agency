48b483550618af3e3aba5cb29213d2cf1b6f0f25
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 14:30:59 +0100

    nadia

--- src/pages/Planpage.js
+++ src/pages/Planpage.js
@@ -1,9 +1,17 @@
 import "../styles/plan.css";
+import {IoIosArrowDown} from 'react-icons/io';
 export default function Plan() {
 
     return(
         <section>
-                    <h1 className="header-plan">Din rejse rute</h1>
+            <h1 className="header-plan">Din rejse rute</h1>
+                   
+            <h4 className="under-rubrik-plan"> tekst...</h4>
+
+            <button className="dage-knap">Dage <IoIosArrowDown/></button> 
+            <button className="dage-knap">Dage <IoIosArrowDown/></button>
+
+            <h3>Mandag den 28. Oktober</h3>
         </section>
     )
 }
