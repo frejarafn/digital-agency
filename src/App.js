85bf1d194ead50c0894266ff5784099558f83376
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 10:54:51 +0100

    nadia

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import HomePage from "./pages/HomePage.js";
 import DiscoverPage from './pages/DiscoverPage.js';
+import PlanPage from './pages/PlanPage.js';
+import SettingsPage from './pages/SettingsPage.js';
 import Attraction from './pages/Attraction';
 import AttractionItem from './pages/AttractionItem';
 
@@ -13,8 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/discover" element={<DiscoverPage />} />
-        <Route path="/plan" element={<HomePage />} />
-        <Route path="/settings" element={<HomePage />} />
+        <Route path="/plan" element={<PlanPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
         <Route path="/attractions" element={<Attraction />} />
         <Route path="/attractionitem/:ItemId" element={<AttractionItem />} />
 
