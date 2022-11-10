2f5f0309bfc67d26854d6e17cf507e110ddd4169
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 20:44:10 +0100

    nadia

--- src/pages/Settingspage.js
+++ src/pages/Settingspage.js
@@ -6,16 +6,16 @@ export default function Settings() {
     return(
         <section>
                 <Header/>
-                    <h1 className="header-settings">Indstillinger & indhold</h1>
+                    <h1 className="header-settings">Indstillinger & Kontakt</h1>
                         <hr className="linje"></hr>
 
                 <div className="setting">
-                    <p className="settings-tekst">Skift sprog <button className="translate-knap"> Engelsk <IoIosArrowForward className="højre-pil1"/></button></p>
+                    <p className="settings-tekst">Skift Sprog <button className="translate-knap"> Dansk <IoIosArrowForward className="højre-pil1"/></button></p>
                     <hr className="linje"></hr>
                 </div>
 
                 <div className="setting">
-                    <p className="settings-tekst">Abonnere til vores nyhedsbreve <IoIosArrowForward className="højre-pil2"/></p> 
+                    <p className="settings-tekst">Tilmeld dig vores nyhedsbrev <IoIosArrowForward className="højre-pil2"/></p> 
                         <hr className="linje"></hr>
                 </div>
 
