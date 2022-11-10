4b1a8460996d529ed71ea0c3469103ab829474ac
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 14:14:24 +0100

    nadia

--- src/pages/DiscoverPage.js
+++ src/pages/DiscoverPage.js
@@ -3,7 +3,29 @@ export default function Discover() {
 
     return (
         <section>
-            <h1>Vi anbefaler</h1>
+
+            <h1 className="header-discover">Vi anbefaler</h1>
+        
+            <h4 className="under-rubrik-discover">Tekst..</h4>
+
+            <div class="row-discover">
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+            </div>
+
+            <div class="row-discover">
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+            </div>
+
+            <div class="row-discover">
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+            </div>
+        
         </section>
     )
 }
--- src/pages/Planpage.js
+++ src/pages/Planpage.js
@@ -3,7 +3,7 @@ export default function Plan() {
 
     return(
         <section>
-                    <h1>Din rejse rute</h1>
+                    <h1 className="header-plan">Din rejse rute</h1>
         </section>
     )
 }
