9b88f512fbd984c594b1d4d288b7c8fba6ef1670
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 13:43:38 +0100

    nadia

--- src/pages/Settingspage.js
+++ src/pages/Settingspage.js
@@ -1,9 +1,31 @@
 import "../styles/setting.css";
+import {IoIosArrowForward} from 'react-icons/io';
+
 export default function Settings() {
 
     return(
         <section>
-                    <h1>Indstillinger & indhold</h1>
+                    <h1 className="header-settings">Indstillinger & indhold</h1>
+                        <hr className="linje"></hr>
+
+                <div>
+                    <p className="settings-tekst">Change language</p>
+                        <button className="translate-knap"> English <IoIosArrowForward/></button>
+                    <hr className="linje"></hr>
+                </div>
+
+                <div>
+                    <p className="settings-tekst">Abonnere til vores nyhedsbreve</p> 
+                    <IoIosArrowForward className="højre-pil"/>
+                        <hr className="linje"></hr>
+                </div>
+
+                <div>
+                    <p className="settings-tekst">Om VisitDenmark</p> 
+                    <IoIosArrowForward className="højre-pil"/>
+                        <hr className="linje"></hr>
+                </div>
+
         </section>
     )
 }
