07fd8fd4ce56c59bf1bb2268f94aa9bee696d2f9
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 22:35:26 +0100

    nadia

--- src/pages/DiscoverPage.js
+++ src/pages/DiscoverPage.js
@@ -1,32 +1,38 @@
 import "../styles/discover.css";
 import Header from "../components/Header";
+import {HiSearch} from 'react-icons/hi';
 export default function Discover() {
 
     return (
         <section>
             <Header/>
             <h1 className="header-discover">Vi anbefaler</h1>
+            <h4 className="under-rubrik-discover">Søg efter seværdigheder, aktiviteter, transport, hotel og meget mere. Tilføj de forskellige aktiviteter til din tidsplan ved at klikke på hjertet.</h4>
         
-            <h4 className="under-rubrik-discover">Tekst..</h4>
-
-            <div class="row-discover">
-                <div class="column-discover"> billede</div>
-                <div class="column-discover"> billede</div>
-                <div class="column-discover"> billede</div>
-            </div>
-
-            <div class="row-discover">
-                <div class="column-discover"> billede</div>
-                <div class="column-discover"> billede</div>
-                <div class="column-discover"> billede</div>
-            </div>
-
-            <div class="row-discover">
-                <div class="column-discover"> billede</div>
-                <div class="column-discover"> billede</div>
-                <div class="column-discover"> billede</div>
-            </div>
+        <form className="søgefelt">
+            <HiSearch className="søg-ikon"/>
+            <input type="text" placeholder="Søg" name="search"/>
+        </form>
         
+        <section>
+            <div class="row-discover">
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+            </div>
+
+            <div class="row-discover">
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+            </div>
+
+            <div class="row-discover">
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+                <div class="column-discover"> billede</div>
+            </div>
+        </section>
         </section>
     )
 }
