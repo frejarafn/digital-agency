400fe128cc34df70ed50b109c6aa7415e0e3ac6c
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-10 10:41:04 +0100

    nadia

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,18 @@
 import "../navigation.css";
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import {MdHome} from 'react-icons/md';
 import {TbSearch} from 'react-icons-/TbSearch';
 import {AiFillCalendar} from 'react-icons/md';
+import {IoIosSettings} from 'react-icons/io';
 export default function Navbar() {
 
     return (
-        <nav>
+        <nav className="navbar">
             <NavLink to="/" end><MdHome/></NavLink>
-            <NavLink to="/discover" end><TbSearch/></NavLink>
-            <NavLink to="/plan" end><AiFillCalendar/></NavLink>
-            <NavLink to="/settings" end><MdHome/></NavLink>
+            <NavLink to="/discover"><TbSearch/></NavLink>
+            <NavLink to="/plan"><AiFillCalendar/></NavLink>
+            <NavLink to="/settings"><IoIosSettings/></NavLink>
         </nav>
     )
 }
