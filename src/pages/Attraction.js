e2517a4b12bdc6efe628e720919bd1d7299a950b
Enebye <Michelle-Chantal@hotmail.com>
2022-11-11 13:08:17 +0100

    Michelle

--- src/pages/Attraction.js
+++ src/pages/Attraction.js
@@ -1,11 +1,13 @@
 
-
+import Top from '../components/Top';
 import AttractionSection from "../components/AttractionSection";
 
 export default function Attraction() {
   return (
     <>
-      <AttractionSection></AttractionSection>
+      <AttractionSection>
+        <Top/> 
+      </AttractionSection>
 
 
     </>
