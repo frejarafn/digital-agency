a3d4f7e4a1accd61f076c0a869b4219914b2831c
Enebye <Michelle-Chantal@hotmail.com>
2022-11-13 14:37:51 +0100

    Michelle

---
+++ src/components/Filtrering.js
@@ -0,0 +1,13 @@
+// text til sorteringsmuligheder
+
+import React from "react"; 
+import '../styles/filtrering.css';
+
+export default function Filtrering(props) {
+
+    return(
+        <h6>
+              {props.name}
+        </h6>
+    )
+}; 
--- src/pages/Attraction.js
+++ src/pages/Attraction.js
@@ -1,10 +1,11 @@
-
-
+import React from "react"; 
 import AttractionSection from "../components/AttractionSection";
+import Top from '../components/Top';
 
 export default function Attraction() {
   return (
     <>
+    <Top /> 
       <AttractionSection></AttractionSection>
 
 
