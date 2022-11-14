68161405fd5e83025605d16330c54a7dfd8aa504
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-11-14 09:12:10 +0100

    activities page errors

--- src/pages/ActivitiesPage.js
+++ src/pages/ActivitiesPage.js
@@ -1,37 +1,48 @@
 import "../styles/activities.css";
 import Header from "../components/Header";
 import Søgefelt from "../components/søgefelt";
-import {MdKayaking} from 'react-icons/md';
+import { MdKayaking } from 'react-icons/md';
 export default function activities() {
 
     return (
         <section>
-            <Header/>
-             {/*intro*/}
-                <div className="activities">
-                    <MdKayaking className="activities-ikon2"/>
-                    <h1 className="header-activities">  Aktiviteter</h1>
+            <Header />
+            {/*intro*/}
+            <div className="activities">
+                <MdKayaking className="activities-ikon2" />
+                <h1 className="header-activities">  Aktiviteter</h1>
+            </div>
+
+            <Søgefelt />
+
+            {/*main indhold*/}
+            <section>
+                <div class="row-activities">
+                    <div class="column-activities"> billede</div>
+                    <div class="column-activities"> billede</div>
                 </div>
 
-        <Søgefelt/>
+                <form className="søgefelt">
+                    <HiSearch className="søg-ikon" />
+                    <input type="text" placeholder="Søg" name="search" />
+                </form>
 
-         {/*main indhold*/}
-        <section>
-            <div class="row-activities">
-                <div class="column-activities"> billede</div>
-                <div class="column-activities"> billede</div>
-            </div>
+                <section>
+                    <div class="row-activities">
+                        <div class="column-activities"> billede</div>
+                        <div class="column-activities"> billede</div>
+                    </div>
 
-            <div class="row-activities">
-                <div class="column-activities"> billede</div>
-                <div class="column-activities"> billede</div>
-            </div>
+                    <div class="row-activities">
+                        <div class="column-activities"> billede</div>
+                        <div class="column-activities"> billede</div>
+                    </div>
 
-            <div class="row-activities">
-                <div class="column-activities"> billede</div>
-                <div class="column-activities"> billede</div>
-            </div>
-        </section>
-        </section>
-    )
+                    <div class="row-activities">
+                        <div class="column-activities"> billede</div>
+                        <div class="column-activities"> billede</div>
+                    </div>
+                </section>
+            </section>
+            )
 }
