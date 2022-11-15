ff9a3b95d0ea83f7d6ffa582a6362c36d8137fda
Nadia-Anjum <diaanjum@outlook.dk>
2022-11-15 10:10:25 +0100

    nadia

--- src/components/PlaceAttractions.js
+++ src/components/PlaceAttractions.js
@@ -8,13 +8,12 @@ export default function PlaceAttraction({ place }) {
         <div>
 
         <div className="at-events">
-                <div className="column-at"> <img src={place.ImageUrl} alt="sdfsdf" className="billede1" /></div>
-                <div className="column-at"> <img src={place.ImageUrl} alt="sdfsdf" className="billede1" /></div>
-        </div>
-        <div className="at-events">
+                <div className="column-at"> <img src={place.ImageUrl} alt="sdfsdf" className="billede1" />
                 <div className="column-at"> {place.Name}</div>
-                <div className="column-at"> {place.Name}</div>
+                </div>
+                <div className="column-at"> </div>
         </div>
+       
 
             </div>
         {/* <div style={{float:"left", width:"75%"}}>{place.Category.Name}</div> */}
