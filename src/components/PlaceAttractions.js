6686e514c737c1d3018c4b6a1520aca75e5785cd
frejarafn <90176537+frejarafn@users.noreply.github.com>
2022-11-15 10:37:30 +0100

    forsøg på centrering

--- src/components/PlaceAttractions.js
+++ src/components/PlaceAttractions.js
@@ -7,14 +7,14 @@ export default function PlaceAttraction({ place }) {
 
         <div>
 
-        <div className="at-events">
+            <div className="at-events">
                 <div className="column-at"> <img src={place.ImageUrl} alt="sdfsdf" className="billede1" />
-                <div className="column-at"> <h4 className="overskrifter1">{place.Name}</h4>
-                </div> </div>
+                    <div className="column-at" id="overskrifth4"> <h4 className="overskrifter1">{place.Name}</h4>
+                    </div> </div>
                 <div className="column-at"> </div>
-        </div>
-
             </div>
+
+        </div>
         {/* <div style={{float:"left", width:"75%"}}>{place.Category.Name}</div> */}
     </div>;
 }
